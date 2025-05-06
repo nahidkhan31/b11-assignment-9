e01aa2ef679e64ff08a5d95492924ad2941e9659
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-06 14:29:36 +0600

    added code

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,10 +1,14 @@
+import { Box } from "lucide-react";
 import React from "react";
 import { NavLink } from "react-router";
 
 const NavBar2 = () => {
   return (
     <div className="">
-      <h2>Subscription Box</h2>
+      <div className="flex gap-2">
+        <Box className="text-green-600" size={50} />
+        <h2 className="text-2xl font-bold">Subscription Box</h2>
+      </div>
       <div className="flex flex-col justify-center gap-5 mt-10">
         <NavLink to="/">Boxes</NavLink>
         <NavLink to="/work">How It Works</NavLink>
